ecf114896c56422f1e85954208ec83018c9d0500
Евгений Шведенко < @DESKTOP-KPJGE1U>
2021-10-15 21:34:16 +0300

    git

---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp1
+{
+    [Validation(18)]
+    public class User
+    {
+        [Required]
+        [StringLength(50, MinimumLength=3)]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public int Age { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/Validation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+  public class Validation : System.Attribute
+        {
+            public int Age { get; set; }
+     
+            public Validation()
+            { }
+     
+            public Validation(int age)
+            {
+                Age = age;
+            }
+        }
+
+}
